c610483ee7d1e5791b1cdc4e9271bfaaa68a4679
fvxstx <faut.btorres@gmail.com>
2026-06-08 20:06:30 -0300

    fix: comentario

--- Front/ast_nodes.py
+++ Front/ast_nodes.py
@@ -16,7 +16,7 @@ class VarDeclNode(ASTNode):
 @dataclass
 class AssignNode(ASTNode):
     var_name: str
-    value: str  # For now, we'll keep expressions as raw strings
+    value: str 
 
 @dataclass
 class PrintNode(ASTNode):
